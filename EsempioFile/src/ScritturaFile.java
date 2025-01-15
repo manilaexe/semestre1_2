2ffedf59dfb33eae6fff0525b2216786e32d516c
Manila <78729862+manilaexe@users.noreply.github.com>
2025-01-15 10:47:43 +0100

    Update ScritturaFile.java

--- EsempioFile/src/ScritturaFile.java
+++ EsempioFile/src/ScritturaFile.java
@@ -5,9 +5,9 @@ public class ScritturaFile {
 	public static void main(String[] args) {
 		
 		try (FileWriter writer = new FileWriter("prova.txt")){
-			writer.write("Ciao a tutti vi odio dal profondo, non sapete fare il vostro lavoro tornate da dove siete arrivati grazie:)\n");
+			writer.write("Ciao a tutti prima riga:)\n");
 			writer.write("LOL pure un'altra riga epico");
-			System.out.println("File scritto con successo (non come te, tu sei un fallito)");
+			System.out.println("File scritto con successo");
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.out.println("Errore: "+e.getMessage());
